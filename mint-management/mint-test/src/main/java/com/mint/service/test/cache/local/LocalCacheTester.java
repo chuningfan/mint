12db7b598b687a9503427f867bb80bd22b60c1b7
Chu Ningfan <chuqingning@163.com>
2019-08-28 22:25:26 +0800

    update test code

--- mint-management/mint-test/src/main/java/com/mint/service/test/cache/local/LocalCacheTester.java
+++ mint-management/mint-test/src/main/java/com/mint/service/test/cache/local/LocalCacheTester.java
@@ -2,14 +2,13 @@ package com.mint.service.test.cache.local;
 
 import java.util.concurrent.TimeUnit;
 
-import com.mint.service.cache.exception.LocalCacheException;
 import com.mint.service.cache.support.CacheOperator;
 import com.mint.service.cache.support.local.LocalCacheFactory;
 import com.mint.service.cache.support.local.LocalCacheType;
 
 public class LocalCacheTester {
 	
-	public static void main(String[] args) throws LocalCacheException, InterruptedException {
+	public static void main(String[] args) throws InterruptedException {
 		CacheOperator<String, Object> cache = LocalCacheFactory.create("test", LocalCacheType.MEMORY, 15L, TimeUnit.SECONDS, 10240L);
 		cache.store("testKey", 123, null, null);
 		Thread.sleep(3000L);

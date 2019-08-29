2fc4cb4555797f573093b7e732bce9603b86e24d
Vic.Chu <ningfan.chu@activenetwork.com>
2019-08-29 13:18:02 +0800

    add actuator support

--- mint-management/mint-user/src/main/java/com/mint/service/user/UserStarter.java
+++ mint-management/mint-user/src/main/java/com/mint/service/user/UserStarter.java
@@ -3,10 +3,12 @@ package com.mint.service.user;
 
 import org.springframework.boot.SpringApplication;
 
+import com.mint.service.annotation.EnableActuator;
 import com.mint.service.annotation.MintService;
 import com.mint.service.database.annotation.EnableJpaOps;
 import com.mint.service.user.metadata.UserServiceMetadataProvider;
 
+@EnableActuator
 @MintService(metadataProvider = UserServiceMetadataProvider.class)
 @EnableJpaOps(basePackages="com.mint.service.user.dao", entityScanPackages="com.mint.service.user.entity")
 public class UserStarter 

8f5f443821b636618eb6984621747982ee960ba2
Vic.Chu <ningfan.chu@activenetwork.com>
2019-08-31 23:03:30 +0800

    move service center to mint service infrastructure

--- mint-management/mint-service-infrastructure/mint-service-registry/src/main/java/com/mint/service/registry/ServiceRegistryStarter.java
+++ mint-management/mint-service-infrastructure/mint-service-registry/src/main/java/com/mint/service/registry/ServiceRegistryStarter.java
@@ -1,4 +1,4 @@
-package com.mint.service.center;
+package com.mint.service.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

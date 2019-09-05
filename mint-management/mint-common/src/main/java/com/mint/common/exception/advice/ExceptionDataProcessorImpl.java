ab053a750ed8f6327dcdbca0a6e8520e932e3570
francis <francis.fang@activenetwork.com>
2019-09-05 17:35:01 +0800

    add log for unexcept exception

--- mint-management/mint-common/src/main/java/com/mint/common/exception/advice/ExceptionDataProcessorImpl.java
+++ mint-management/mint-common/src/main/java/com/mint/common/exception/advice/ExceptionDataProcessorImpl.java
@@ -1,12 +1,15 @@
 package com.mint.common.exception.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.mint.common.exception.MintException;
 
 public class ExceptionDataProcessorImpl implements ExceptionDataProcessor {
-
+	private final static Logger logger = LoggerFactory.getLogger(ExceptionDataProcessorImpl.class);
 	@Override
 	public void process(MintException e) {
-		
+		logger.error("mint error {}",e);
 	}
 
 }
